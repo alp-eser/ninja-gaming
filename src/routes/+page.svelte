c2ad756fc6d2be7936561911aa30640f00855651
alpeser <alpeser@visight.com.tr>
2023-02-12 19:40:54 +0300

    bruch up on Svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let title = "Ninja Gaming Guides";
+
+  const updateTitle = () => {
+    title = "Something completely different";
+  };
+</script>
+
+<div class="index">
+  <h2>{title}</h2>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam in commodi
+    dignissimos quaerat facilis? Velit sed neque ipsa voluptatibus saepe
+    asperiores dolores, doloribus at similique. Commodi adipisci ducimus ratione
+    earum!
+  </p>
+  <input type="text" bind:value={title} />
+  <button on:click={updateTitle}>Change Title</button>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
