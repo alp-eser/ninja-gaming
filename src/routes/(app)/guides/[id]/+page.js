06c54ea88e4dcc59eaaecbe81fe5d3cf13b6f05c
alp.eser <alp.eser@bosphorusiss.com>
2023-03-02 17:56:23 +0300

    implement route parameters

---
+++ src/routes/(app)/guides/[id]/+page.js
@@ -0,0 +1,14 @@
+export async function load({ fetch, params }) {
+  const id = params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const guide = await res.json();
+  if (res.ok) {
+    return {
+      guide: guide,
+    };
+  }
+  return {
+    status: res.status,
+    error: new Error("Could not fetch guide"),
+  };
+}
